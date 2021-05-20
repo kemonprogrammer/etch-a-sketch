36fe0d9075dd1f3c2ae586c01b561f1a0ff9196e
Michael Luu <michaelluu02@protonmail.com>
2021-05-21 01:18:44 +0200

    Style boxes with CSS-grid to display as 16x16 grid
    ~
    ~
    ".git/COMMIT_EDITMSG" 14L, 297C

--- main.js
+++ main.js
@@ -2,14 +2,11 @@ const boxgrid = document.getElementById("boxgrid");
 
 for (let i = 1; i <= 16; i++) {
 
-    const row = document.createElement("div")
-    boxgrid.appendChild(row);
-    row.classList.add("row" + i.toString());
     for (let j = 1; j <= 16; j++) {
         const box = document.createElement("div");
-        row.appendChild(box);
         box.classList.add("col" + j.toString());
+        box.classList.add("row" + i.toString());
         box.textContent = ((i-1) * 16 + j).toString();
+        boxgrid.appendChild(box);
     }
-    boxgrid.appendChild(document.createElement("br"));
 }
