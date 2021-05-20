0f03da09abd84e3ac86f5872c8d97af4b973af75
Michael Luu <michaelluu02@protonmail.com>
2021-05-21 01:57:10 +0200

    Add permanent color change when hovered
    ~
    ~
    ".git/COMMIT_EDITMSG" 14L, 297C

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ for (let i = 1; i <= 16; i++) {
         const box = document.createElement("div");
         box.classList.add("col" + j.toString());
         box.classList.add("row" + i.toString());
-        box.textContent = ((i-1) * 16 + j).toString();
+        box.addEventListener("mouseenter", () => {
+            box.style.backgroundColor = "rgb(173, 201, 226)";
+        })
         boxgrid.appendChild(box);
     }
 }
