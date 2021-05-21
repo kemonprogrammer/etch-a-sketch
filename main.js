01bda431b64b534408a0b423c15f684c50e7f3bc
Michael Luu <michaelluu02@protonmail.com>
2021-05-21 10:39:23 +0200

    Add new Resolution button
    ~
    ~
    ".git/COMMIT_EDITMSG" 14L, 297C

--- main.js
+++ main.js
@@ -1,14 +1,46 @@
-const boxgrid = document.getElementById("boxgrid");
+const boxGrid = document.getElementById("boxgrid");
+const button = document.getElementById("resolution");
 
-for (let i = 1; i <= 16; i++) {
+const gridWidth = 960;
 
-    for (let j = 1; j <= 16; j++) {
-        const box = document.createElement("div");
-        box.classList.add("col" + j.toString());
-        box.classList.add("row" + i.toString());
-        box.addEventListener("mouseenter", () => {
-            box.style.backgroundColor = "rgb(173, 201, 226)";
-        })
-        boxgrid.appendChild(box);
+
+var drawCanvas = function(boxesPerSide) {
+    for (let i = 1; i <= boxesPerSide; i++) {
+        for (let j = 1; j <= boxesPerSide; j++) {
+            const box = document.createElement("div");
+            box.classList.add("col" + j.toString());
+            box.classList.add("row" + i.toString());
+            boxGrid.appendChild(box);
+
+            box.addEventListener("mouseenter", () => {
+                box.style.backgroundColor = "rgb(173, 201, 226)";
+            });
+        }
     }
+    boxGrid.style.display = "grid";
+    const boxWidth = gridWidth / boxesPerSide;
+    boxGrid.style.gridTemplate = `repeat(${boxesPerSide}, ${boxWidth}px) / repeat(${boxesPerSide}, ${boxWidth}px)`;
 }
+
+var clearCanvas = function() {
+    boxGrid.textContent = "";
+    boxGrid.removeAttribute("style");
+}
+
+var askResolution = () => {
+    const res = prompt("Enter resolution size", "10");
+    if (res === null) return;
+    if (res >= 100) {
+        alert("Number too big, max: 100");
+        askResolution();
+        return;
+    }
+    clearCanvas();
+    drawCanvas(res);
+}
+
+button.addEventListener("click", askResolution);
+
+
+
+drawCanvas(16);
