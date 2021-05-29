f3e5b33cdc40e7b85f8fe2f4ab2de4100516676f
Michael Luu <michaelluu02@protonmail.com>
2021-05-29 22:26:36 +0200

    Add working slider to change gridsize

--- main.js
+++ main.js
@@ -1,67 +1,66 @@
 const boxGrid = document.getElementById("boxgrid");
-const button = document.getElementById("resolution");
 const colorChooser = document.getElementById("colorChooser");
+const resolutionSlider = document.getElementById("resolutionSlider");
+const resolutionValue = document.getElementById("resolutionValue");
 
 const gridWidth = 960;
 
-var random = function(number) {
-    // random number from 0 to number
-    return Math.floor(Math.random() * (number + 1));
+const random = function(number) {
+  // random number from 0 to number
+  return Math.floor(Math.random() * (number + 1));
 }
 
-
-var drawCanvas = function(boxesPerSide) {
-    for (let i = 1; i <= boxesPerSide; i++) {
-        for (let j = 1; j <= boxesPerSide; j++) {
-            const box = document.createElement("div");
-            box.classList.add("col" + j.toString());
-            box.classList.add("row" + i.toString());
-            box.classList.add("l100");
-
-            boxGrid.appendChild(box);
-
-            box.addEventListener("mouseenter", () => {
-                if (colorChooser.value === "cl") {
-                    box.style.backgroundColor = `hsl(${random(360)}, ${random(100)}%, ${random(100)}%)`;
-                } else {
-                    // pick lXX for lightness value
-                    let className = String(box.classList);
-                    let lightness = +className.slice(className.lastIndexOf("l") + 1, className.length);
-                    if (lightness > 0) {
-                        box.classList.remove(`l${lightness}`);
-                        lightness -= 10;
-                        box.classList.add(`l${lightness}`);
-                    }
-                    box.style.backgroundColor = `hsl(0, 0%, ${lightness}%)`;
-                }
-            });
-        }
+const drawBox = e => {
+  const box = e.target;
+  if (colorChooser.value === "cl") {
+    box.style.backgroundColor = `hsl(${random(360)}, ${random(100)}%, ${random(100)}%)`;
+  } else {
+    // pick lXX for lightness value
+    let className = String(box.classList);
+    let lightness = +className.slice(className.lastIndexOf("l") + 1, className.length);
+    if (lightness > 0) {
+      box.classList.remove(`l${lightness}`);
+      lightness -= 10;
+      box.classList.add(`l${lightness}`);
     }
-    const boxWidth = gridWidth / boxesPerSide;
-
-    boxGrid.style.display = "grid";
-    boxGrid.style.gridTemplate = `repeat(${boxesPerSide}, ${boxWidth}px) / repeat(${boxesPerSide}, ${boxWidth}px)`;
+    box.style.backgroundColor = `hsl(0, 0%, ${lightness}%)`;
+  }
 }
 
-var clearCanvas = function() {
-    boxGrid.textContent = "";
-    boxGrid.removeAttribute("style");
-}
-
-var askResolution = () => {
-    const res = prompt("Enter resolution size", "10");
-    if (res === null) return;
-    if (res > 100) {
-        alert("Number too big, max: 100");
-        askResolution();
-        return;
+const drawCanvas = function(boxesPerSide) {
+  for (let i = 1; i <= boxesPerSide; i++) {
+    for (let j = 1; j <= boxesPerSide; j++) {
+      const box = document.createElement("div");
+      box.classList.add("col" + j.toString());
+      box.classList.add("row" + i.toString());
+      // l100 for lightness 100%
+      box.classList.add("l100");
+      boxGrid.appendChild(box);
+      box.addEventListener("mouseenter", drawBox);
     }
-    clearCanvas();
-    drawCanvas(res);
+  }
+  const boxWidth = gridWidth / boxesPerSide;
+
+  boxGrid.style.display = "grid";
+  boxGrid.style.gridTemplate = `repeat(${boxesPerSide}, ${boxWidth}px) / repeat(${boxesPerSide}, ${boxWidth}px)`;
 }
 
+var clearCanvas = () => {
+  boxGrid.textContent = "";
+  boxGrid.removeAttribute("style");
+}
 
-button.addEventListener("click", askResolution);
+resolutionSlider.value = "16";
 
+resolutionSlider.oninput = () => {
+  // display value while sliding
+  resolutionValue.textContent = resolutionSlider.value;
+}
+
+resolutionSlider.onchange = () => {
+  // change grid after mouseup
+  clearCanvas();
+  drawCanvas(resolutionSlider.value);
+}
 
 drawCanvas(16);
